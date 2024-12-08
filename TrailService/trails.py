a097090de28da5104ae4dfddcff64309508c1d72
Dennis-Stefan Toader <146251437+dennisnpc@users.noreply.github.com>
2024-12-08 20:52:31 +0000

    Created function and endpoint to add trail

--- TrailService/trails.py
+++ TrailService/trails.py
@@ -4,3 +4,11 @@ from models import Trail, trails_schema
 def read_all_trails():
     trails = Trail.query.all()
     return trails_schema.dump(trails)
+
+def create_trail():
+    request_json = request.get_json()
+    trail_data = request_json.get("Trail")
+    new_trail = Trail(**trail_data)
+    db.session.add(new_trail)
+    db.session.commit()
+    return trails_schema.dump(new_trail)
